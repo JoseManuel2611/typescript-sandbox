9e2c5140beec1127a9039203451189518463b5da
JoseManu <josemanuelescolano1@gmail.com>
2025-06-23 19:28:28 +0200

    ENTREGA 1 CENA DE AMIGOS

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,4 @@ import "./style.css";
 //Entrega 1
 //Cena de amigos, 6 amigos, ticket de 120€ y 18€ de bebidas invitadas por el anfitrión
 
-console.log("cada persona pagará: " + ((120 - 18) / 6) + " habiendo descontado las bebidas"); 
+console.log("cada persona pagará: " + ((120 - 18) / 6) + " habiendo descontado las bebidas"); //Cantidad total a pagar menos las bebidas invitadas por el anfitrión, dividido entre el número de amigos
