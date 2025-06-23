0150d0e3a6cd8e2b92be13d308ef7b6ab32a6381
JoseManu <josemanuelescolano1@gmail.com>
2025-06-23 19:21:20 +0200

    ENTREGA 1 CENA AMIGOS

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 
-console.log("Hello Typescript!");
-console.log(2 + 2);
-console.log("2" + 2);
+//Entrega 1
+//Cena de amigos, 6 amigos, ticket de 120€ y 18€ de bebidas invitadas por el anfitrión
+
+console.log("cada persona pagará: " + ((120 - 18) / 6) + " habiendo descontado las bebidas");
