a2e3f6458e8b320cfedd9ac1d8d9024f777d5c6b
JoseManu <josemanuelescolano1@gmail.com>
2025-06-25 18:14:38 +0200

    TYPESCRIPT

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,5 @@ import "./style.css";
 //Cena de amigos, 6 amigos, ticket de 120€ y 18€ de bebidas invitadas por el anfitrión
 
 console.log("cada persona pagará: " + ((120 - 18) / 6) + " habiendo descontado las bebidas"); //Cantidad total a pagar menos las bebidas invitadas por el anfitrión, dividido entre el número de amigos
+
+//Pruebas TS
