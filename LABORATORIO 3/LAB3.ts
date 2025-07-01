1ee64245eac80703dc55e68db3075ac76ef582d2
JoseManu <josemanuelescolano1@gmail.com>
2025-07-01 17:26:48 +0200

    LABORATORIO 3

--- LABORATORIO 3/LAB3.ts
+++ LABORATORIO 3/LAB3.ts
@@ -31,7 +31,7 @@ const grupo2 : Grupo = {
 const grupo3 : Grupo = {
     Nombre: "AC DC",
     Fundacion: 1973,
-    Activo: false,
+    Activo: true,
     Genero: `${HardRock}`,
 };
 
@@ -49,10 +49,34 @@ const grupo5 : Grupo = {
     Genero: `${Rock}`,
 };
 
+
 // Mostrar los grupos musicales en consola
-console.log("%cGrupos musicales:", "color: black; font-size: 20px; font-weight: bold; font-style: italic;");
-console.log(grupo1);
-console.log(grupo2);
-console.log(grupo3);
-console.log(grupo4);
-console.log(grupo5);
+console.log(
+  "%c" + grupo1.Nombre,
+  "font-weight: bold; font-size: 20px; color: green;"
+);
+console.log(`Año de fundación: ${grupo1.Fundacion} | Activo: ${grupo1.Activo ? "Sí" : "No"} | Género: ${grupo1.Genero}\n`);
+
+console.log(
+  "%c" + grupo2.Nombre,
+  "font-weight: bold; font-size: 20px; color: green;"
+);
+console.log(`Año de fundación: ${grupo2.Fundacion} | Activo: ${grupo2.Activo ? "Sí" : "No"} | Género: ${grupo2.Genero}\n`);
+
+console.log(
+  "%c" + grupo3.Nombre,
+  "font-weight: bold; font-size: 20px; color: green;"
+);
+console.log(`Año de fundación: ${grupo3.Fundacion} | Activo: ${grupo3.Activo ? "Sí" : "No"} | Género: ${grupo3.Genero}\n`);
+
+console.log(
+  "%c" + grupo4.Nombre,
+  "font-weight: bold; font-size: 20px; color: green;"
+);
+console.log(`Año de fundación: ${grupo4.Fundacion} | Activo: ${grupo4.Activo ? "Sí" : "No"} | Género: ${grupo4.Genero}\n`);
+
+console.log(
+  "%c" + grupo5.Nombre,
+  "font-weight: bold; font-size: 20px; color: green;"
+);
+console.log(`Año de fundación: ${grupo5.Fundacion} | Activo: ${grupo5.Activo ? "Sí" : "No"} | Género: ${grupo5.Genero}\n`);
